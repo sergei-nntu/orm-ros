62779c46fbfdcbf62102df95e5d8d14a19850fd5
Maksim Kropanov <kropanov@gmail.com>
2024-12-20 13:08:53 +0300

    chore: add fixme

--- src/we_r2_arm_moveit/scripts/utils.py
+++ src/we_r2_arm_moveit/scripts/utils.py
@@ -3,6 +3,7 @@ import array as ar
 
 
 def joints_state_validation(joint_positions, expected_joint_count):
+    # FIXME: remove list
     if not isinstance(joint_positions, (list, np.ndarray, ar.array)):
             raise ValueError("joint_positions must be a list or numpy array or array.")
     elif len(joint_positions) != expected_joint_count:
